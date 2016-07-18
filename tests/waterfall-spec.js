c1633021a3936242b9d2052d885d6ab43d6fa595
eenewbsauce <ryan.m.tracey@gmail.com>
2016-07-18 16:47:25 -0400

    test(waterfall.js) - ensure non-blocking

--- tests/waterfall-spec.js
+++ tests/waterfall-spec.js
@@ -3,11 +3,53 @@ var should = chai.should();
 var expect = chai.expect;
 var waterfall = require('../waterfall');
 
+before
+
 describe('waterfall', function() {
-	it ('should return a promise', function() {
+	it ('should return a promise', () => {
 		let wfOutput = waterfall();
 
 		expect(wfOutput).to.be.a('Promise');
 		expect(wfOutput.then).to.be.a('function');
 	}); 
+
+	it ('should return the sum of all resolves', (done) => {
+		waterfall(getPromises()).then(sum => {
+			expect(sum).to.equal(6);
+			done();
+		});
+	});
+
+	it('should not be blocking', (done) => {
+		let promises = getPromises();
+		let resolvedPromise = false;
+
+		promises.unshift(generatePromise().then(() => {
+			resolvedPromise = true;
+		}));
+
+		setTimeout(() => {
+			expect(resolvedPromise).to.be.false;
+			done();
+		}, 98);
+
+		waterfall(promises).then(sum => {
+			expect(sum).to.equal(6);
+			done();
+		});		
+	});
 });
+
+function getPromises() {
+	return [{res: 1, to: 100}, {res: 2,  to: 50}, {res: 3, to: 150}].map(item => {
+		return generatePromise(item.res, item.to);
+	});
+}
+
+function generatePromise(resolveValue = 1, timeout = 100) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve(resolveValue)
+		}, timeout);
+	});
+}
--- waterfall.js
+++ waterfall.js
@@ -1,26 +1,3 @@
-let p1 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		console.log('p1 resolved');
-		resolve(1);
-	}, 300);
-});
-
-let p2 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		console.log('p2 resolved');
-		resolve(2);
-	}, 600);
-});
-
-let p3 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		console.log('p3 resolved');
-		resolve(3);
-	}, 900);
-});
-
-let promiseArr = [p1, p2, p3];
-
 module.exports = function waterfall(promises) {
 	return new Promise((resolve, reject) => {
 		let pIndex = 0;
@@ -49,23 +26,3 @@ module.exports = function waterfall(promises) {
 		, 0);
 	});
 }
-
-setTimeout(() => {
-	console.log('priority 1')
-}, 0);
-
-setTimeout(() => {
-	console.log('priority 2')
-}, 298);
-
-
-setTimeout(() => {
-	console.log('priority 3')
-}, 301);
-
-// waterfall(promiseArr).then(result => {
-// 	console.log(result);
-// }).catch(err => {
-// 	console.log(err);
-// });
-
