6f1c823fdd80021a301437629a4feaec05a09a79
eenewbsauce <ryan.m.tracey@gmail.com>
2016-08-10 08:19:53 -0400

    test(promise): add test for multiple then() cbs

--- tests/promise-spec.js
+++ tests/promise-spec.js
@@ -7,7 +7,7 @@ var expect = chai.expect;
 var P = require('../es6Promise');
 
 describe('promise', () => {  
-  it('should have have a then method and resolve() should route to it', (done) => {
+  it('should have have a then method and resolve() should route to it', done => {
     var p = new P((resolve, reject) => {
       setTimeout(() => {
         resolve("resolved");
@@ -23,7 +23,7 @@ describe('promise', () => {
       });
   });  
   
-  it('should have have a catch method and reject() should route to it', (done) => {
+  it('should have have a catch method and reject() should route to it', done => {
     var p = new P((resolve, reject) => {
       setTimeout(() => {
         reject("rejected");
@@ -39,7 +39,7 @@ describe('promise', () => {
       });
   });  
   
-  it('should have have a then method that takes resolve/reject callbacks and routes correctly', (done) => {
+  it('should have have a then method that takes resolve/reject callbacks and routes correctly', done => {
     var pRes = new P((resolve, reject) => {
       setTimeout(() => {
         resolve("resolved");
@@ -67,6 +67,34 @@ describe('promise', () => {
     setTimeout(() => {
       done();
     }, 15)
-  });  
+  }); 
+  
+  it('should allow multiple then cbs and route properly', done => {
+    let resolvedCount = 0;
+    var p = new P((resolve, reject) => {
+      setTimeout(() => {
+        resolve(1);
+      }, 5);
+    });
+    
+    p
+      .then(res => {
+        resolvedCount += res;
+      })
+      .then(res => {
+        resolvedCount += res;
+      });
+      
+    p
+      .catch(() => {})
+      .then(res => {
+        resolvedCount += res;
+      })
+      
+    setTimeout(() => {
+      expect(resolvedCount).to.equal(3);
+      done();
+    }, 8)
+  }); 
 });
 
